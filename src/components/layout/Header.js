0ff4da791b3f44f6133f9f607ac41c8c7cf8b574
Nguyen Viet Van <nguyenvietvan2002@gmail.com>
2023-12-17 05:10:05 +0700

    feat: upadate

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -78,26 +78,19 @@ const { token, userId, loading } = useSelector(state => state.auth)
               <MDBNavbarLink href="cart">
                 <MDBIcon fas icon="shopping-cart" />
               </MDBNavbarLink>
-              <MDBNavbarLink href="notification">
-                <MDBIcon fas icon="bell" />
-              </MDBNavbarLink>
-
-              <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-                <MDBBtn href="/auth/login">LOGIN</MDBBtn>
-                <MDBBtn outline href="/auth/Register">
-                  SIGNUP
-                </MDBBtn>
-              </div>
+              
             </div>
           </MDBContainer>
         </MDBNavbar>
       </div>
 
-      {/* <div className="user">
+      <div className="user">
             
             {token ? (
                        <div>
-                        <Link to='/notification' id='notification' ><svg id='notificationIcon' xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S19.4 416 32 416H416c12.6 0 24-7.4 29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384 226.8V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm45.3 493.3c12-12 18.7-28.3 18.7-45.3H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z"/></svg></Link>
+                                 <MDBNavbarLink href="notification">
+                <MDBIcon fas icon="bell" />
+              </MDBNavbarLink>
                         <div className="dropdown">
                         <Link to = {`/course/create/list/${userId}`} id='create-course' onClick={myCourseHandle} >My Course</Link>
                                 <figure className="avatar avatar-nav">
@@ -127,12 +120,19 @@ const { token, userId, loading } = useSelector(state => state.auth)
                     ) : 
                     
                     !loading &&  <div id = "head-login">
-                                         <Link to ='/auth/login' >Login</Link>
+                              
+
+              <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                <MDBBtn href="/auth/login">LOGIN</MDBBtn>
+                <MDBBtn outline href="/auth/Register">
+                  SIGNUP
+                </MDBBtn>
+              </div>
                                      </div> 
                                      }
                       
            
-        </div> */}
+        </div>
     </Fragment>
   );
 };
