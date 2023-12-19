30655b913ed356ca81811511ac1d4ed076dfd6a9
VanNemui <van.na205045@sis.hust.edu.vn>
2023-12-19 21:54:53 +0700

    header login

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { useAlert } from "react-alert";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
@@ -28,15 +28,14 @@ import { useEffect } from "react";
 const Header = () => {
   const alert = useAlert();
   const dispatch = useDispatch();
-const { token, userId, loading } = useSelector(state => state.auth)
- //lấy token được lưu gọi hàm loadUser và lấy dữ liệu
+  const { token, userId, loading } = useSelector((state) => state.auth);
+  //lấy token được lưu gọi hàm loadUser và lấy dữ liệu
 
   useEffect(() => {
-      dispatch(loadUser(userId, token));   
+    dispatch(loadUser(userId, token));
   }, [userId, token]);
- const { userinfo } = useSelector(state => state.info)
- console.log("dsfdfds", userinfo)
-
+  const { userinfo } = useSelector((state) => state.info);
+  console.log("dsfdfds", userinfo);
 
   const logoutHandler = () => {
     dispatch(logout());
@@ -48,9 +47,10 @@ const { token, userId, loading } = useSelector(state => state.auth)
   };
   const myCourseHandle = () => {
     dispatch(myCourses(userId, token));
-    alert.success('Here is your courses')
-  }
-  
+    alert.success("Here is your courses");
+  };
+  const [openNav, setOpenNav] = useState(false);
+
   return (
     <Fragment>
       <div className="d-flex justify-content-between">
@@ -61,16 +61,39 @@ const { token, userId, loading } = useSelector(state => state.auth)
         >
           <MDBContainer className="inline-row d-flex justify-content-between">
             <MDBNavbarNav className="d-flex flex-row">
-              <MDBNavbarBrand href="#" className="row flex">
+              <MDBNavbarBrand href="/" className="row flex">
                 <Link to="/">
                   <MDBIcon className="ms-1" size="2x" fab icon="aviato" />
                 </Link>
               </MDBNavbarBrand>
             </MDBNavbarNav>
             <div className="d-grid gap-4 d-md-flex justify-content-md-end align-items-center">
-              <Link to="/">
-                <h5 className="align-items-end">Home</h5>
-              </Link>
+              <MDBNavbar expand="lg" >
+                <MDBContainer fluid>
+                  <MDBNavbarBrand href="/">Home</MDBNavbarBrand>
+                  <MDBNavbarToggler
+                    type="button"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    onClick={() => setOpenNav(!openNav)}
+                  >
+                    <MDBIcon icon="bars" fas />
+                  </MDBNavbarToggler>
+                  <MDBCollapse navbar open={openNav}>
+                    <MDBNavbarNav>
+                      <MDBNavbarItem>
+                        <MDBNavbarLink href="course">Course</MDBNavbarLink>
+                      </MDBNavbarItem>
+                      <MDBNavbarItem>
+                        <MDBNavbarLink href="blog">Blog</MDBNavbarLink>
+                      </MDBNavbarItem>
+                      <MDBNavbarItem>
+                        <MDBNavbarLink href="contact">Contact</MDBNavbarLink>
+                      </MDBNavbarItem>
+                    </MDBNavbarNav>
+                  </MDBCollapse>
+                </MDBContainer>
+              </MDBNavbar>
               <div>
                 <Search />
               </div>
@@ -78,60 +101,83 @@ const { token, userId, loading } = useSelector(state => state.auth)
               <MDBNavbarLink href="cart">
                 <MDBIcon fas icon="shopping-cart" />
               </MDBNavbarLink>
-              
+              <MDBNavbarLink href="notification">
+                <MDBIcon fas icon="bell" />
+              </MDBNavbarLink>
+
+              <div className="user">
+                {token ? (
+                  <div>
+                    <div className="dropdown">
+                      <Link
+                        to={`/course/create/list/${userId}`}
+                        id="create-course"
+                        onClick={myCourseHandle}
+                      >
+                        My Course
+                      </Link>
+                      <figure className="avatar avatar-nav">
+                        <img
+                          alt={userinfo && userinfo.name}
+                          className="rounded-circle"
+                        />
+                      </figure>
+                      <span className="head-user-name">
+                        {userinfo && userinfo.name}
+                      </span>
+
+                      <div
+                        className="dropdown-menu"
+                        aria-labelledby="dropDownMenuButton"
+                      >
+                        {userinfo && userinfo.role === "ADMIN" && (
+                          <Link
+                            className="dropdown-item"
+                            id="head-item"
+                            to="/dashboard"
+                          >
+                            Dashboard
+                          </Link>
+                        )}
+                        <Link
+                          className="dropdown-item"
+                          id="head-item"
+                          to={`/courses/enrolled/${userId}`}
+                          onClick={myEnrollCourseHandle}
+                        >
+                          MyEnrollCourses
+                        </Link>
+                        <Link
+                          className="dropdown-item"
+                          id="head-item"
+                          to={`/user/${userId}`}
+                        >
+                          Profile
+                        </Link>
+                        <Link
+                          className="dropdown-item text-danger"
+                          id="head-item-logout"
+                          to="/"
+                          onClick={logoutHandler}
+                        >
+                          Logout
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  !loading && (
+                    <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                      <MDBBtn href="/auth/login">LOGIN</MDBBtn>
+                      <MDBBtn href="/auth/Register">SIGNUP</MDBBtn>
+                    </div>
+                  )
+                )}
+              </div>
             </div>
           </MDBContainer>
         </MDBNavbar>
       </div>
-
-      <div className="user">
-            
-            {token ? (
-                       <div>
-                                 <MDBNavbarLink href="notification">
-                <MDBIcon fas icon="bell" />
-              </MDBNavbarLink>
-                        <div className="dropdown">
-                        <Link to = {`/course/create/list/${userId}`} id='create-course' onClick={myCourseHandle} >My Course</Link>
-                                <figure className="avatar avatar-nav">
-
-                                    <img
-                                        alt={userinfo && userinfo.name}
-                                        className="rounded-circle"
-                                    />
-                                </figure>
-                                <span className='head-user-name'>{userinfo && userinfo.name}</span>
-
-                            <div className="dropdown-menu" aria-labelledby="dropDownMenuButton">
-
-                                {userinfo && userinfo.role === 'ADMIN' && (
-                                    <Link className="dropdown-item" id="head-item" to="/dashboard">Dashboard</Link>
-                                )}
-                                <Link className="dropdown-item" id="head-item" to={`/courses/enrolled/${userId}`}  onClick={myEnrollCourseHandle} >MyEnrollCourses</Link>
-                                <Link className="dropdown-item" id="head-item" to={`/user/${userId}`} >Profile</Link>
-                                <Link className="dropdown-item text-danger" id="head-item-logout" to="/" onClick={logoutHandler}>
-                                    Logout
-                                </Link>
-                            </div>
-                        </div>
-                      
-                        </div>
-                    ) : 
-                    
-                    !loading &&  <div id = "head-login">
-                              
-
-              <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-                <MDBBtn href="/auth/login">LOGIN</MDBBtn>
-                <MDBBtn outline href="/auth/Register">
-                  SIGNUP
-                </MDBBtn>
-              </div>
-                                     </div> 
-                                     }
-                      
-           
-        </div>
     </Fragment>
   );
 };
