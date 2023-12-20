6236779825813a246ad2f82dea733e780419a61e
VanNemui <van.na205045@sis.hust.edu.vn>
2023-12-21 03:06:08 +0700

    enroll update

--- src/pages/course/MyEnrollCourseDetails.js
+++ src/pages/course/MyEnrollCourseDetails.js
@@ -1,111 +1,148 @@
-import React, { Fragment, useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { useAlert } from 'react-alert'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { Fragment, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useAlert } from "react-alert";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  MDBTypography,
+  MDBBtn,
+  MDBTextArea,
+  MDBInput,
+  MDBCheckbox,
+} from "mdb-react-ui-kit";
 
-import ListReviews from '../review/ListReviews'
-import Loader from '../../components/layout/Loader'
-import MetaData from '../../components/layout/MetaData'
-import { getCourseDetails, newFeedback, clearErrors, getSections } from '../../actions/courseActions'
+import ListReviews from "../review/ListReviews";
+import Loader from "../../components/layout/Loader";
+import MetaData from "../../components/layout/MetaData";
+import {
+  getCourseDetails,
+  newFeedback,
+  clearErrors,
+  getSections,
+} from "../../actions/courseActions";
 const MyEnrollCourseDetails = () => {
+  const { token } = useSelector((state) => state.auth);
 
-    const { token } = useSelector(state => state.auth)
+  const dispatch = useDispatch();
+  const alert = useAlert();
+  const { id } = useParams();
+  const { error, loading, course } = useSelector(
+    (state) => state.courseDetails
+  );
+  const { sections } = useSelector((state) => state.sections);
+  console.log("df", course);
+  console.log("ssss", sections);
 
-    const dispatch = useDispatch();
-    const alert = useAlert();
-    const {id} = useParams();
-    const { error, loading, course } = useSelector(state => state.courseDetails)
-    const { sections } = useSelector(state => state.sections)
-   console.log("df", course)
-   console.log("ssss", sections)
+  const [rating, setRating] = useState("");
+  const [time, setTime] = useState("");
+  const [feed_back, setFeed_back] = useState("");
 
-   const [rating, setRating] = useState('');
-   const [time, setTime] = useState('');
-   const [feed_back, setFeed_back] = useState('');
-
-    useEffect(() => {
-        dispatch(getCourseDetails(id, token));
-        if (error) {
-            alert.error(error);
-            dispatch(clearErrors())
-        }
-    }, [dispatch, id, token])
-    useEffect(() => {
-        dispatch(getSections(id, token));
-        console.log('ffff', id, token);
-        }, [dispatch, id, token])
-    const feedback = (e) => {
-        const formData = new FormData();
-        formData.set('rating', rating);
-        formData.set('time', time);
-        formData.set('feed_back', feed_back);
-        formData.set('course_id', id);
-
-        const jsonObject = {};
-        for (const pair of formData.entries()) {
-            jsonObject[pair[0]] = pair[1]
-        }
-        const jsonData = JSON.stringify(jsonObject);
-
-        dispatch(newFeedback(jsonData, token));
-        alert.success('Sent your feedback')
+  useEffect(() => {
+    dispatch(getCourseDetails(id, token));
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
     }
+  }, [dispatch, id, token]);
+  useEffect(() => {
+    dispatch(getSections(id, token));
+    console.log("ffff", id, token);
+  }, [dispatch, id, token]);
+  const feedback = (e) => {
+    const formData = new FormData();
+    formData.set("rating", rating);
+    formData.set("time", time);
+    formData.set("feed_back", feed_back);
+    formData.set("course_id", id);
 
-    
-    return (
+    const jsonObject = {};
+    for (const pair of formData.entries()) {
+      jsonObject[pair[0]] = pair[1];
+    }
+    const jsonData = JSON.stringify(jsonObject);
+
+    dispatch(newFeedback(jsonData, token));
+    alert.success("Sent your feedback");
+  };
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
         <Fragment>
-            {loading ? <Loader /> : (
-                <Fragment>
-                    <MetaData title={course.learningObject} />
-                    <div className="courses-details">
-
-                        <div className="course-details-name">
-                            <h3>{course.learningObject}</h3>
-                            <p id="course_id">Course # {course.id}</p>
-
-                            <hr />
-
-                            <div className="course-rating">
-                                <div className="star" style={{ width: `${(course.rating / 5) * 100}%` }}></div>
-                             {/* {[...Array(course.rating)].map((_, index) => (
+          <MetaData title={course.learningObject} />
+          <div className="courses-details">
+            <div className="course-details-name">
+              <div className="p-3 mb-2 bg-primary bg-gradient text-white rounded-5">
+                <h3>{course.learningObject}</h3>
+                <p id="course_id">Course # {course.id}</p>
+              </div>
+              <hr />
+              <div className="course-rating">
+                <div
+                  className="star"
+                  style={{ width: `${(course.rating / 5) * 100}%` }}
+                ></div>
+                {/* {[...Array(course.rating)].map((_, index) => (
                              <i key={index} className="fas fa-star"></i>
                             ))} */}
-                            </div>
+              </div>
 
-                            <hr />
-
-                            <p id="course_price">${course.price}</p>
-                            <hr />
-
-                            <p id="requiredSkills">${course.requiredSkills}</p>
-                            <p id="courseFor">${course.courseFor}</p>
-                            <p id="title">${course.title}</p>
-                            <p id="subtitle">${course.subtitle}</p>
-                            <p id="language">${course.language}</p>
-                            <p id="level">${course.level}</p>
-                            <p id="category">${course.category}</p>
-                            <p id="primarilyTaught">${course.primarilyTaught}</p>
-                            <p id="welcomeMessage">${course.welcomeMessage}</p>
-                            <p id="congratulationMessage">${course.congratulationMessage}</p>
-                            <hr />
-                            <p id="status">${course.status}</p>
-                            <h4 className="mt-2">Description:</h4>
-                            <p>{course.courseDescription}</p>
-                            <hr />
-                            <p id="createdAt">${course.createdAt}</p>
-                            <p id="updatedAt">${course.updatedAt}</p>
-                            <hr />
-                           {/* khi click vào section thì thả xuống các lecture */}
-                           {/* {sections.map(section => (
+              <hr />
+              <MDBTypography className="lead mb-0 border">
+                <p>Description:</p>
+                <p>{course.courseDescription}</p>
+              </MDBTypography>
+              <hr />
+              <p id="course_price">${course.price}</p>
+              <p id="requiredSkills">${course.requiredSkills}</p>
+              <p id="courseFor">${course.courseFor}</p>
+              <p id="title">${course.title}</p>
+              <p id="subtitle">${course.subtitle}</p>
+              <p id="language">${course.language}</p>
+              <p id="level">${course.level}</p>
+              <p id="category">${course.category}</p>
+              <p id="primarilyTaught">${course.primarilyTaught}</p>
+              <p id="welcomeMessage">${course.welcomeMessage}</p>
+              <p id="congratulationMessage">${course.congratulationMessage}</p>
+              <p id="status">${course.status}</p>
+              <p id="createdAt">${course.createdAt}</p>
+              <p id="updatedAt">${course.updatedAt}</p>
+              <hr />
+              {/* khi click vào section thì thả xuống các lecture */}
+              {/* {sections.map(section => (
                               <p id="section">${section.name}</p>
                            ))} */}
-                            <button type="button"  >discussion</button>
-
-
-
-
-
-                          <div className="form-group">
+              <MDBBtn>Discussion</MDBBtn>
+              <form>
+                <h4>Đánh giá của bạn</h4>
+                <MDBInput
+                  wrapperClass="mb-4"
+                  textarea
+                  id="form4Example3"
+                  rows={4}
+                  label="Feedback:"
+                  value={feed_back}
+                  onChange={(e) => setFeed_back(e.target.value)}
+                />
+                <MDBInput
+                  id="form4Example1"
+                  wrapperClass="mb-4"
+                  label="Khóa học được đánh giá bao nhiêu sao?"
+                  value={rating}
+                  onChange={(e) => setRating(e.target.value)}
+                />
+                <MDBCheckbox
+                  wrapperClass="d-flex justify-content-center mb-4"
+                  id="form4Example4"
+                  label="Send me a copy of this feedback"
+                  defaultChecked
+                />
+                <MDBBtn type="submit" className="mb-4" block onClick={feedback}>
+                  Feedback
+                </MDBBtn>
+              </form>
+              {/* <div className="form-group">
                                     <label htmlFor="name_field">Đánh giá của bạn</label>
                                     <input
                                         type="text"
@@ -124,23 +161,17 @@ const MyEnrollCourseDetails = () => {
                             <button type="button" onClick={feedback} >feedback</button>
         
 
-                            <hr />
+                            <hr /> */}
+            </div>
+          </div>
 
-
-
-                          
-
-                        </div>
-                    </div>
-
-                    {course.reviews && course.reviews.length > 0 && (
-                        <ListReviews reviews={course.reviews} />
-                    )}
-
-                </Fragment>
-            )}
+          {course.reviews && course.reviews.length > 0 && (
+            <ListReviews reviews={course.reviews} />
+          )}
         </Fragment>
-    )
-}
+      )}
+    </Fragment>
+  );
+};
 
-export default MyEnrollCourseDetails
+export default MyEnrollCourseDetails;
