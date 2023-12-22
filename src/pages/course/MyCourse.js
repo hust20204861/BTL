56139ca30beb1e64bd24c805b5426a1f03b3d7fe
Nguyen Viet Van <nguyenvietvan2002@gmail.com>
2023-12-23 02:11:15 +0700

    update-2h-23/12

--- src/pages/course/MyCourse.js
+++ src/pages/course/MyCourse.js
@@ -1,43 +1,94 @@
-import React from "react";
+import React, { Fragment} from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import {
-  MDBCard,
-  MDBCardHeader,
-  MDBCardBody,
-  MDBCardTitle,
-  MDBCardText,
-  MDBBtn,
-} from "mdb-react-ui-kit";
-
+import { Box, Button, List, ListItem, Rating, Typography } from "@mui/material";
+import { COLOR } from "../../styles/color";
+import MetaData from "../../components/layout/MetaData";
 const MyCourse = () => {
   const { mycourses } = useSelector((state) => state.mycourses);
   return (
-    <MDBCard
-      shadow="8"
-      border="primary"
-      background="white"
-      className="align-items-center justify-content-center mb-4 mt-4 w-50"
-      alignment="center"
-      style={{ height: "200px", margin: "360px" }}
-    >
-      <MDBCardHeader>My Course</MDBCardHeader>
-      <MDBCardBody>
-        <MDBCardTitle>Create Your Course</MDBCardTitle>
-        <Link to="/create/course">CREATE</Link>
-        {mycourses.map((course) => (
-          <div className="course" key={course.id}>
-            <div className="course-name">
-              <h5 className="cart-title">
-                <Link to={`/mycourse/${course.id}`}>
-                  {course.learningObject}
-                </Link>
-              </h5>
-            </div>
-          </div>
-        ))}
-      </MDBCardBody>
-    </MDBCard>
+    <Fragment>
+    <MetaData title={'My Course'} />
+    <Box>
+    <Typography variant="h2" sx={{ mb: '1rem' }} textAlign={"center"}>
+    Khóa học của bạn
+    </Typography>
+    <Box
+    display="flex"
+    flexDirection="row"
+    flexWrap="wrap"
+    justifyContent="space-between"
+  >
+   
+    {mycourses.map((course, index) => (
+      <Box
+        key={index}
+        width="30%"
+        position="relative"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        border={2}
+        borderColor={COLOR.gray}
+        borderRadius={2}
+        padding={2}
+        marginBottom={2}
+      >
+        <Typography variant="h4" color={COLOR.black}>
+          {course.title}
+        </Typography>
+        <Box
+          component="img"
+          sx={{
+            height: 190,
+            width: "100%",
+         
+          }}
+          alt="Course Image"
+          src={course.courseImageUrl}
+        />
+        <Box
+          display="flex"
+          flexDirection="column"
+          textAlign="left"
+          width="100%"
+        >
+          {/* <Box display="flex" alignItems="center" marginTop={4}>
+            <Typography
+              variant="body1"
+              color={COLOR.functionYellow.default}
+              fontWeight={600}
+              marginRight={1}
+            >
+              {course.rating}
+            </Typography>
+            <Rating
+              name="half-rating"
+              defaultValue={parseFloat(course.rating)}
+              precision={0.5}
+              readOnly
+            />
+          </Box> */}
+        </Box>
+        <Link to = {`/mycourse/${course.id}`}>
+        <Button variant="contained" fullWidth sx={{ marginTop: "12px" }}>
+          <Typography variant="body1" color={COLOR.white}>
+            Xem khóa học
+          </Typography>
+        </Button>
+        </Link>
+      </Box>
+    ))}
+  </Box>
+  <Link  to="/create/course">
+  <Button variant="contained" fullWidth sx={{ marginTop: "20px", padding: "20px" }}>
+          <Typography variant="body1" color={COLOR.white}>
+            Tạo khóa học của bạn
+          </Typography>
+        </Button>
+  </Link>
+  </Box>
+  </Fragment>
   );
 };
 
