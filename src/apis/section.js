5255abb79eecd50fb91a61bc29403b8013e77709
minhtuyenvp02 <minhtuyenvp02@gmail.com>
2023-12-24 15:05:56 +0700

    Add curriculum page

--- src/apis/section.js
+++ src/apis/section.js
@@ -12,4 +12,47 @@ const getSectionFromCourse = async (courseId, accessToken) => {
   return data;
 };
 
-export { getSectionFromCourse };
+const updateSection = async ({
+  sectionId,
+  courseId,
+  sectionData,
+  accessToken,
+}) => {
+  const { data } = await api({
+    method: "PUT",
+    url: `/section/${sectionId}/${courseId}`,
+    data: sectionData,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return data;
+};
+
+const createSection = async ({ courseId, sectionData, accessToken }) => {
+  const { data } = await api({
+    method: "POST",
+    url: `/section/${courseId}`,
+    data: sectionData,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return data;
+};
+
+const deleteSection = async ({ sectionId, courseId, accessToken }) => {
+  const { data } = await api({
+    method: "DELETE",
+    url: `/section/${sectionId}/${courseId}`,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return data;
+};
+
+export { getSectionFromCourse, updateSection, deleteSection, createSection };
---
+++ src/containers/TeacherCourse/CourseCurriculum.jsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useRef, useState } from "react";
+import { getAccessToken } from "../../apis/auth";
+import {
+  createSection,
+  deleteSection,
+  getSectionFromCourse,
+} from "../../apis/section";
+import { getLectureByCourseIdAndSectionId } from "../../apis/lecture";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import CoursePartCard from "./CoursePartCard";
+import CourseCurriculumSection from "./CourseCurriculumSection";
+import SuccessMessage from "../../components/SuccessMessage";
+
+const CourseCurriculum = () => {
+  const courseId = window.location.pathname.split("/")[3];
+  const [courseData, setCourseData] = useState([]);
+  const fetchData = async () => {
+    try {
+      const accessToken = await getAccessToken();
+      const response = await getSectionFromCourse(courseId, accessToken);
+      let course = [];
+      const getData = response.map(async (item) => {
+        const res = await getLectureByCourseIdAndSectionId(
+          courseId,
+          item.id,
+          accessToken
+        );
+        course.push({
+          sectionId: item.id,
+          sectionName: item.name,
+          lecture: res.data,
+        });
+      });
+      await Promise.all(getData);
+      setCourseData(course.sort((a, b) => a.sectionId - b.sectionId));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleDeleteSection = async (sectionId) => {
+    try {
+      const accessToken = await getAccessToken();
+      const resData = await deleteSection({
+        sectionId,
+        courseId,
+        accessToken,
+      });
+      console.log(resData);
+      setCourseData(courseData.filter((item) => item.sectionId !== sectionId));
+      SuccessMessage("Success", "Delete section successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleAddSection = async () => {
+    try {
+      const accessToken = getAccessToken();
+      const resData = await createSection({
+        sectionData: {
+          name: "New Section",
+        },
+        courseId,
+        accessToken,
+      });
+      setCourseData(courseData.push(resData.data));
+      SuccessMessage("Success", "Delete section successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Box width={"100%"} paddingX={4} paddingY={2}>
+      {courseData.map((item, index) => (
+        <CourseCurriculumSection
+          sectionData={item}
+          sectionIndex={index}
+          handleDeleteSection={handleDeleteSection}
+        />
+      ))}
+      <Box width={"100%"} textAlign={"right"}>
+        <Button
+          variant={"contained"}
+          color={"primary"}
+          onClick={handleAddSection}
+        >
+          Create new section
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default CourseCurriculum;
